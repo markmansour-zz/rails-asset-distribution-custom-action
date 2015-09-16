733b65ef6e9220917fd6fa6a6bb10e6bc860640e
Mark Mansour <markmans@amazon.com>
2015-09-16 21:02:46 +0000

    Fix to allow POSTS to work

--- app/controllers/job_workers_controller.rb
+++ app/controllers/job_workers_controller.rb
@@ -1,4 +1,6 @@
 class JobWorkersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def create
     index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get 'job_workers/index'
-
-  root 'job_workers#index'
-  resources :job_workers
+#  root 'job_workers#index'
+  resources :job_workers, :path => '/'
 end
